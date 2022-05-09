056c0e3e18dccbd88822418b5cc5cec35135e273
Milo Moisson <milomoisson@gmail.com>
2022-05-09 21:00:47 +0200

    feat: use u128 to get higher Fibonacci numbers

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -3,16 +3,16 @@ use std::io::stdin;
 use std::process::exit;
 
 fn main() {
-	println!("Quel nombre de la suite de fibonacci voulez vous obtenir ?");
+	println!("Quel nombre de la suite de Fibonacci voulez vous obtenir ?");
 
-	const MAX_FIBONACCI_NUMBER: u32 = 92;
+	const MAX_FIBONACCI_NUMBER: u128 = 185;
 	let mut user_number = String::new();
 
 	stdin()
 		.read_line(&mut user_number)
 		.expect("Un problème est survenu...");
 
-	let user_number: u32 = match user_number.trim().parse() {
+	let user_number: u128 = match user_number.trim().parse() {
 		Ok(number) => number,
 		Err(_) => {
 			eprintln!("Vous devez entrez un nombre !");
@@ -20,26 +20,21 @@ fn main() {
 		}
 	};
 
-	match user_number.cmp(&MAX_FIBONACCI_NUMBER) {
-		Ordering::Less => {}
-		Ordering::Equal => {}
-		Ordering::Greater => {
-			println!("Ce nombre est trop grand !");
-			exit(1)
-		}
+	if let Ordering::Greater = user_number.cmp(&MAX_FIBONACCI_NUMBER) {
+		println!("Ce nombre est trop grand !");
+		exit(1);
 	}
 
-	let mut current_number: u64 = 0;
-	let mut next_number: u64 = 1;
+	let mut current_number: u128 = 0;
+	let mut next_number: u128 = 1;
+
 	for _ in 0..(user_number) {
 		println!("{}", &current_number);
 
 		let tmp = next_number;
-		next_number = next_number + current_number;
+		next_number += current_number;
 		current_number = tmp;
 	}
 
 	println!("Le {}ème nombre est {}", &user_number, &current_number);
-
-	exit(0)
 }
