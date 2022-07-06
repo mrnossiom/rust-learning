4dd31e91a5f84c0b79b93ed64f1f07c60a4ed92d
Milo Moisson <milomoisson@gmail.com>
2022-07-06 12:59:11 +0200

    feat(no_name_suite): add code for generation

--- no_name_suite/src/main.rs
+++ no_name_suite/src/main.rs
@@ -1,3 +1,29 @@
+const START: &str = "1";
+
 fn main() {
-	println!("Hello, no name suite!");
+	let mut last = START.to_string();
+
+	for i in 0..100 {
+		println!("Chars: {}\nGen: {i}\n{}", last.len(), last);
+
+		last = advance(&last);
+	}
+}
+
+fn advance(base: &String) -> String {
+	let mut chars = base.chars().peekable();
+	let mut new = String::new();
+
+	let mut count = 1;
+	while let Some(char) = chars.next() {
+		if char == *chars.peek().unwrap_or(&' ') {
+			count += 1;
+		} else {
+			new.push_str(&count.to_string());
+			new.push(char);
+			count = 1;
+		}
+	}
+
+	new
 }
